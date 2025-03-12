715a9471c4c62e09b6f35d63012ba47aa89da153
Fellas <neuneu12@naver.com>
2025-03-12 14:18:39 +0900

    .

---
+++ pybo/urls.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from django.urls import include, path
+from pybo import views
+
+urlpatterns = []
--- pybo/views.py
+++ pybo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+
+# http://127.0.0.1:8000/hello/
