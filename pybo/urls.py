546e15c9895a7f4639e8ed3255b688c5a3f87c7a
Fellas <neuneu12@naver.com>
2025-03-14 14:48:46 +0900

    .

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 from pybo import views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.index),
+]
--- pybo/views.py
+++ pybo/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from pybo.models import Question
+
 # Create your views here.
 
 # http://127.0.0.1:8000/hello/
 
 
 def index(request):
-    return HttpResponse("<h1>안녕하세요.</h1> </br>게시판을 만들겠습니다.")
-
-
-def hello(request):
-    return HttpResponse("<h1>홍길동 입니다.</h1>")
+    # Question 모델의 데이터를 create_date 기준으로 내림차순 정렬하여 가져오는 것
+    question_list = Question.objects.order_by("-create_date")
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
