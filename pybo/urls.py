3c5dca65abc2f6e9c27e1cd8ee8d448511d1d404
Fellas <neuneu12@naver.com>
2025-03-17 10:19:56 +0900

    질문 상세

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 from pybo import views
 
+## http://127.0.0.1:8000/pybo
 urlpatterns = [
     path("", views.index),
+    path("<int:question_id>/", views.detail), # /3
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -10,3 +10,10 @@ def index(request):
     question_list = Question.objects.order_by("-create_date")
     context = {"question_list": question_list}
     return render(request, "pybo/question_list.html", context)
+
+# http://127.0.0.1:8000/pybo/<int:question_id/>
+def detail(request, question_id):
+    question = Question.objects.get(id=question_id)
+
+    context = {"question": question}
+    return render(request, "pybo/question_detail.html", context)
