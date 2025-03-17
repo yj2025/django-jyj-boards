0d17e61c271c5b978342443cf746356eb2f9822f
Fellas <neuneu12@naver.com>
2025-03-17 11:14:43 +0900

    url

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 from pybo import views
 
+app_name = "pybo"
+
 ## http://127.0.0.1:8000/pybo
 urlpatterns = [
-    path("", views.index),
-    path("<int:question_id>/", views.detail), # /3
+    path("", views.index, name='index'),
+    ## http://127.0.0.1:8000/pybo/<int:question_id>/
+    path("<int:question_id>/", views.detail, nmae="detail"), # /3
 ]
