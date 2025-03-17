781fc11c2c6bf1b45f5c44ac5ea10e2c0bb7c33c
Fellas <neuneu12@naver.com>
2025-03-17 13:19:19 +0900

    답변등록

--- pybo/views.py
+++ pybo/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
 from django.utils import timezone
-from pybo.models import Question
+from pybo.models import Answer, Question
 
 # Create your views here.
 
@@ -27,7 +27,10 @@ def answer_create(request, question_id):
     content = request.POST.get("content")
 
     # select * from question, answer where answer.question_id = 6
-    question.answer_set.create(content=content, create_date=timezone.now())
+    # question.answer_set.create(content=content, create_date=timezone.now())
+    
+    answer = Answer(question=question, content=content, create_date=timezone.now())
+    answer.save()
 
     return redirect('pybo:detail', question_id=question_id)
 
