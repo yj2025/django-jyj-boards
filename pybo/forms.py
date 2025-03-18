6b1b53dac77affda5bd82eb2988bf28bf3785817
Fellas <neuneu12@naver.com>
2025-03-18 14:22:40 +0900

    .

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from pybo.models import Question
+from pybo.models import Answer, Question
 
 
 # dev_9
@@ -8,3 +8,9 @@ class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question  # form 과 모델을 연결
         fields = ["subject", "content"]  # QuestionForm에서 사용할 Question 모델의 속성
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer  # form 과 모델을 연결
+        fields = ["subject"]  # QuestionForm에서 사용할 Question 모델의 속성
+        labels = {"content": "답변내용"}
