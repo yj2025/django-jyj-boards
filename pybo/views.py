f2fe445c8bcf9423652bf57b553582339e57ef0b
Fellas <neuneu12@naver.com>
2025-03-19 11:18:00 +0900

    .

--- pybo/views.py
+++ pybo/views.py
@@ -13,7 +13,9 @@ from django.core.paginator import Paginator
 def index(request):
 
     page = request.GET.get("page", "1")  # 페이지
+
     question_list = Question.objects.order_by("-create_date")
+    
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
     context = {"question_list": page_obj}
