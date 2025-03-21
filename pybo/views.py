867b854d79b7b324bd46cf0242635e58e3ef91c6
Fellas <neuneu12@naver.com>
2025-03-22 00:18:53 +0900

    .

--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
 
-# Create your views here.
 
-# http://127.0.0.1:8000/hello/
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello World!!")
