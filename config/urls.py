34f57e6632db1393505a89d8bbf9428d55f05f78
Fellas <neuneu12@naver.com>
2025-03-22 00:05:38 +0900

    .

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from django.urls import include, path
-
-from pybo import views
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
