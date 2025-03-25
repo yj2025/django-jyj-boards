c1efcd554fb0ed480dbd7777a9cca72b5ace2c02
Fellas <neuneu12@naver.com>
2025-03-25 11:40:38 +0900

    .

--- pybo/urls.py
+++ pybo/urls.py
@@ -23,4 +23,14 @@ urlpatterns = [
         views.question_delete,
         name="question_delete",
     ),  # dev_17
+    path(
+        "answer/modify/<int:answer_id>/",
+        views.answer_modify,
+        name="answer_modify",
+    ),  # dev_18
+    path(
+        "answer/delete/<int:answer_id>/",
+        views.answer_delete,
+        name="answer_delete",
+    ),  # dev_18
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -107,7 +107,7 @@ def question_modify(request, question_id):
 
         if form.is_valid():
             question = form.save(commit=False)
-            question.mosdify_date = timezone.now()
+            question.modify_date = timezone.now()
             question.save()
             return redirect("pybo:detail", question_id=question.id)
     else:
@@ -127,3 +127,40 @@ def question_delete(request, question_id):
 
     question.delete()
     return redirect("pybo:index")
+
+
+# dev_18
+@login_required(login_url="common:login")
+def answer_modify(request, answer_id):
+
+    answer = get_object_or_404(Answer, pk=answer_id)
+
+    if request.user != answer.author:
+        messages.error(request, "수정 권한이 없습니다.")
+        return redirect("pybo:detail", question_id=answer.question.id)
+
+    if request.method == "POST":
+        form = AnswerForm(request.POST, instance=answer)
+
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect("pybo:detail", question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+
+    context = {"answer": answer, "form": form}
+    return render(request, "pybo/answer_form.html", context)
+
+
+@login_required(login_url="common:login")
+def answer_delete(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+
+    if request.user != answer.author:
+        messages.error(request, "삭제권한이 없습니다")
+    else:
+        answer.delete()
+
+    return redirect("pybo:detail", question_id=answer.question.id)
