81b893c0935342c1aefb7a7f660d125aa1ba6389
Fellas <neuneu12@naver.com>
2025-03-27 09:13:29 +0900

    .

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -8,21 +8,36 @@ from django.utils import timezone
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.db.models import Q
 
 
 # http://127.0.0.1:8000/pybo
+# dev_20 검색 추가
 def index(request):
 
-    print(request.user)
-
-    # ?page=4
+    # ?kw=홍길동&page=1
     page = request.GET.get("page", "1")  # 페이지
+    kw = request.GET.get("kw", "")  # 검색어
 
+    # select * from question , user where ~~~;
     question_list = Question.objects.order_by("-create_date")
+
+    # dev_20
+    if kw:
+        # select disctict * from question,user where subject like "%홍길동%" or
+        question_list = question_list.filter(
+            Q(subject__icontains=kw)  # 제목검색
+            | Q(content__icontains=kw)  # 내용 검색
+            | Q(answer__content__icontains=kw)  # 답변내용 검색
+            | Q(author__username__icontains=kw)  # 질문 글쓴이 검색
+            | Q(answer__author__username__icontains=kw)  # 답변 글쓴이 검색
+        ).distinct()
 
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
-    context = {"question_list": page_obj}
+
+    # dev_20 수정
+    context = {"question_list": page_obj, "page": page, "kw": kw}
 
     return render(request, "pybo/question_list.html", context)
 
